307941975d45cb6d53f38f5cc9afef823f67832c
Vitalii Khalin <beardmenx@gmail.com>
2023-03-22 13:14:03 +0200

    add adaptive

--- js/main.js
+++ js/main.js
@@ -24,6 +24,14 @@ function open(evt) {
     .classList.add('tabs__content-item--active');
 }
 
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+});
+
+// swiper-slider
 const swiper = new Swiper('.swiper', {
   pagination: {
     el: '.swiper-pagination',
